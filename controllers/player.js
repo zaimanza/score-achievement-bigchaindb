03a4fab7ab754528d6ab449d0fca830511829df5
MANZA <36519974+zaimanza@users.noreply.github.com>
2022-05-13 17:22:24 +0800

    structure files

---
+++ controllers/player.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+    res.status(200).json({
+        message: 'Handling GET requests to /products'
+    });
+});
+
+router.post('/', (req, res, next) => {
+    const product = {
+        name: req.body.name,
+        price: req.body.price
+    };
+    res.status(201).json({
+        message: 'Handling POST requests to /products',
+        createdProduct: product
+    });
+});
+
+module.exports = router;
