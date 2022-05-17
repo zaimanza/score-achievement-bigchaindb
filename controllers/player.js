429bab25463e7d5b2552430b9519faf7b4105fb4
MANZA <36519974+zaimanza@users.noreply.github.com>
2022-05-17 11:05:52 +0800

    create user and login done

--- controllers/player.js
+++ controllers/player.js
@@ -1,20 +1,53 @@
 const express = require('express');
+const bip39 = require('bip39');
+const BigChainDB = require('bigchaindb-driver');
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
+router.get('/', async (req, res, next) => {
+    //create
+    const mnemonic = req.body.mnemonic
+    // const mnemonic = bip39.generateMnemonic()
+    console.log("generated_mnemonic")
+    console.log(mnemonic)
+    //create mnemonic
+
+    const seed = (await bip39.mnemonicToSeed(mnemonic)).slice(0, 32)
+    const user = new BigChainDB.Ed25519Keypair(seed)
+
+    await console.log("hello_bigchain_call_start_6")
+    await console.table({
+        mnemonic: mnemonic,
+        private: user.privateKey,
+        public: user.publicKey,
+    })
     res.status(200).json({
-        message: 'Handling GET requests to /products'
+        mnemonic: mnemonic,
+        private: user.privateKey,
+        public: user.publicKey,
     });
 });
 
-router.post('/', (req, res, next) => {
-    const product = {
-        name: req.body.name,
-        price: req.body.price
-    };
-    res.status(201).json({
-        message: 'Handling POST requests to /products',
-        createdProduct: product
+router.post('/', async (req, res, next) => {
+    //create
+    const mnemonic = bip39.generateMnemonic()
+    console.log("generated_mnemonic")
+    console.log(mnemonic)
+    //create mnemonic
+    //   const mnemonic = 'fever loan palace crack hint ivory nice soccer purchase whale air ready'
+
+    const seed = (await bip39.mnemonicToSeed(mnemonic)).slice(0, 32)
+    const user = new BigChainDB.Ed25519Keypair(seed)
+
+    await console.log("hello_bigchain_call_start_6")
+    await console.table({
+        mnemonic: mnemonic,
+        private: user.privateKey,
+        public: user.publicKey,
+    })
+    res.status(200).json({
+        mnemonic: mnemonic,
+        private: user.privateKey,
+        public: user.publicKey,
     });
 });
 
